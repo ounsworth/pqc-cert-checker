2b6e7703af5213bb4ad43abccc85ce52b6d070b9
Mike Ounsworth <mike@ounsworth.ca>
2024-07-21 11:38:16 +1000

    action

--- src/rebuild_results_certs_r3.sh
+++ src/rebuild_results_certs_r3.sh
@@ -3,13 +3,11 @@
 # build a list of all compat matrix files in provider dirs
 dirs=$(ls providers)
 for dir in $dirs; do
-    dir=../providers/$dir
+    dir=providers/$dir
     if [ ! -d $dir ]; then continue; fi
     files+=$(find $dir/compatMatrices/artifacts_certs_r3 -name "*.csv" )
     files+=" "
 done
-
-printf "Files: %s\n" $files
 
 # build the compat matrix
 python3 src/pqc_report_writer_common.py docs/oid_mapping.md pqc_hackathon_results_certs_r3.md Certificate $files
